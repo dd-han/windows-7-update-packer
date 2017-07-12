083432c397d3dc7dc457da6779e8e0265f8888d5
dd-han <han@dd-han.tw>
2017-07-12 15:34:30 +0800

    remove debug message

--- pack_all.cmd
+++ pack_all.cmd
@@ -8,11 +8,8 @@ set TEMP_DRIVER=F:
 for %%N in (1 2 3 4 5) do (
   format %TEMP_DRIVER% /FS:NTFS /Q /Y
   dism /apply-image /imagefile:tw_windows_7_ultimate_with_sp1_x86_dvd_u_677488\sources\install.wim /index:%%N /ApplyDir:%TEMP_DRIVER%\
-  pause
   x86_list.cmd %TEMP_DRIVER%\
-  pause
   Dism /Capture-Image /ImageFile:install_patch_x86.wim /CaptureDir:%TEMP_DRIVER% /Name:%%N
-  pause
 )
 
 for %%N in (1 2 3 4) do (
