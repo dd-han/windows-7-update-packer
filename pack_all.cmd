24d0d3ed3dd2e24b6aaf84a51af403cef969fe89
dd-han <han@dd-han.tw>
2017-07-13 00:09:47 +0800

    fix image be replace

--- pack_all.cmd
+++ pack_all.cmd
@@ -9,14 +9,14 @@ for %%N in (1 2 3 4 5) do (
   format %TEMP_DRIVER% /FS:NTFS /Q /Y
   dism /apply-image /imagefile:tw_windows_7_ultimate_with_sp1_x86_dvd_u_677488\sources\install.wim /index:%%N /ApplyDir:%TEMP_DRIVER%\
   x86_list.cmd %TEMP_DRIVER%\
-  Dism /Capture-Image /ImageFile:install_patch_x86.wim /CaptureDir:%TEMP_DRIVER% /Name:%%N
+  Dism /Capture-Image /ImageFile:install_patch_x86.wim /CaptureDir:%TEMP_DRIVER% /Name:%%N /index:%%n
 )
 
 for %%N in (1 2 3 4) do (
   format %TEMP_DRIVER% /FS:NTFS /Q /Y
   dism /apply-image /imagefile:tw_windows_7_ultimate_with_sp1_x64_dvd_u_677414\sources\install.wim /index:%%N /ApplyDir:%TEMP_DRIVER%\
   x86_list.cmd %TEMP_DRIVER%\
-  Dism /Capture-Image /ImageFile:install_patch_x86.wim /CaptureDir:%TEMP_DRIVER% /Name:%%N
+  Dism /Capture-Image /ImageFile:install_patch_x86.wim /CaptureDir:%TEMP_DRIVER% /Name:%%N /index:%%n
 )
 
 pause
